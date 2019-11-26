1ad29b72088c24c568057b1ac620700b2d49d2ce
BloodDragon2580 <gewinner.thomas@gmail.com>
2019-11-26 12:17:08 +0100

    Update

---
+++ WhatADrop/locale/ruRU.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("WhatADrop", "ruRU")
+if not L then return end
+
+L["WhatADrop: Loaded"] = "WhatADrop: Загрузка"
+L["WhatADrop: Enabled"] = "WhatADrop: Включить"
+L["WhatADrop: Disabled"] = "WhatADrop: Отключить"
+L["WhatADrop does not have any function command."] = "WhatADrop не имеет функциональной команды."
+
+L["This shows the level of the item you'll find in this week's chest."] = "Показывает уровень предмета, который вы получите в сундуке на этой недели."
+L["Weekly Chest Reward"] = "Еженедельная награда за сундук"
+
+L["Loot Item Level: "] = "Уровень предмета: "
+L["Weekly Chest Item Level: "] = "Еженедельный сундук Уровень предмета: "
---
+++ WhatADrop/locale/zhTW.lua
@@ -0,0 +1,17 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("WhatADrop", "zhTW")
+if not L then return end
+
+L["WhatADrop: Loaded"] = "WhatADrop: 已載入"
+L["WhatADrop: Enabled"] = "WhatADrop: 已啟用"
+L["WhatADrop: Disabled"] = "WhatADrop: 已停用"
+L["WhatADrop does not have any function command."] = "WhatADrop 沒有任何功能指令。"
+
+L["This shows the level of the item you'll find in this week's chest."] = "顯示每週寶箱可以開到的獎勵物品等級。"
+L["Weekly Chest Reward"] = "每週寶箱獎勵"
+
+L["Loot Item Level: "] = "戰利品等級: "
+L["Weekly Chest Item Level: "] = "每週寶箱物品等級: "
+
+
+
+
